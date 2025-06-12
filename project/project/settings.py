833421c437f35df352d3a23967a7d03bd53b2a4c
Asfana <katheejathasfana@gmail.com>
2025-06-12 14:56:36 +0530

    updated

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@iwhcos-r^n3v@q#1qgz=r1l8kox#78**@8qsns^@12i+ov@jm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
